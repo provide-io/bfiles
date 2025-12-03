e3bf24756b9bc034a51d5cb5a9165e5c5bcc9707
Tim <code@tim.life>
2025-12-02 16:52:07 -0800

    ️🐝📁 [skip ci] auto-commit

--- tests/fixtures/files/cli_project/subdir/helper.py
+++ tests/fixtures/files/cli_project/subdir/helper.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""TODO: Add module docstring."""
 
 def help_async():
     pass
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""TODO: Add module docstring."""
 
 import hashlib  # For hash checks
 from pathlib import Path
